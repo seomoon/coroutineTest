4ac01a0004460e7edf0375fb80e19669850ae4a3
minji <minji@mobmail.shinhan.com>
2021-05-03 16:32:28 +0900

    project start

---
+++ app/src/main/java/com/shinhan/mobmail/minji/randomuserwithtdd/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shinhan.mobmail.minji.randomuserwithtdd
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
