c58d3ba22cb43fffef72ad94fcd02956438907da
정민지 <s2018s38@gmail.com>
2021-05-05 18:24:31 +0900

    [#14] MainViewModel에 라이브 데이터 만들어서 가져온 UserList 넣기

--- app/src/main/java/com/shinhan/mobmail/minji/randomuserwithtdd/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/shinhan/mobmail/minji/randomuserwithtdd/presentation/main/MainViewModel.kt
@@ -1,7 +1,12 @@
 package com.shinhan.mobmail.minji.randomuserwithtdd.presentation
 
 import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.shinhan.mobmail.minji.randomuserwithtdd.domain.DataResult
+import com.shinhan.mobmail.minji.randomuserwithtdd.domain.entity.User
+import com.shinhan.mobmail.minji.randomuserwithtdd.domain.succeeded
 import com.shinhan.mobmail.minji.randomuserwithtdd.domain.usecase.GetUserListUseCase
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
@@ -10,13 +15,25 @@ class MainViewModel(
     private val getUserListUseCase: GetUserListUseCase
 ): ViewModel() {
 
+    val userList: LiveData<ArrayList<User>> get() = _userList
+    private val _userList = MutableLiveData<ArrayList<User>>()
+
     fun getUserList() {
         getUserListUseCase(10)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
             .subscribe(
-                { Log.i("민지", "SUCCESS: $it")},
-                { Log.i("민지", "FALSE: $it")}
+                {
+                    when(it) {
+                        is DataResult.Success ->
+                            _userList.value = it.data.results
+                        else ->
+                            _userList.value = arrayListOf()
+                    }
+                },
+                {
+
+                }
             ).isDisposed
     }
 }
