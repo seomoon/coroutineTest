5ccba79d5a5dbb2715902762becfea9a4d60aee0
minji <minji@mobmail.shinhan.com>
2021-05-12 15:50:16 +0900

    [RAN-24] ViewModel에서 disposal()이 제대로 사용되지 않는 문제 해결

--- app/src/main/java/com/shinhan/mobmail/minji/randomuserwithtdd/presentation/main/MainViewModel.kt
+++ app/src/main/java/com/shinhan/mobmail/minji/randomuserwithtdd/presentation/main/MainViewModel.kt
@@ -1,19 +1,21 @@
 package com.shinhan.mobmail.minji.randomuserwithtdd.presentation.main
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.shinhan.mobmail.minji.randomuserwithtdd.domain.DataResult
 import com.shinhan.mobmail.minji.randomuserwithtdd.domain.entity.User
-import com.shinhan.mobmail.minji.randomuserwithtdd.domain.succeeded
 import com.shinhan.mobmail.minji.randomuserwithtdd.domain.usecase.GetUserListUseCase
 import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.rxkotlin.addTo
 import io.reactivex.schedulers.Schedulers
 
 class MainViewModel(
     private val getUserListUseCase: GetUserListUseCase
 ) : ViewModel() {
+
+    private val compositeDisposable = CompositeDisposable()
 
     private val _userList = MutableLiveData<ArrayList<User>>()
     val userList: LiveData<ArrayList<User>>
@@ -23,19 +25,19 @@ class MainViewModel(
         getUserListUseCase(10)
             .subscribeOn(Schedulers.io())
             .observeOn(AndroidSchedulers.mainThread())
-            .subscribe(
-                {
-                    when (it) {
-                        is DataResult.Success ->
-                            _userList.value = it.data.results
-                        else ->
-                            _userList.value = arrayListOf()
-                    }
-                    Log.i("민지", "userList : ${_userList.value}")
-                },
-                {
-
+            .subscribe {
+                when (it) {
+                    is DataResult.Success ->
+                        _userList.value = it.data.results
+                    else ->
+                        _userList.value = arrayListOf()
                 }
-            ).isDisposed
+            }.addTo(compositeDisposable)
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+
+        compositeDisposable.dispose()
     }
 }
